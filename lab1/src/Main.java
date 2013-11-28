8751fbff50f189cc2190a194dc23bb8bd7b60ab0
bchen30 <bchen30@my.bcit.ca>
2013-11-28 14:28:46 -0800

    bob

---
+++ lab1/src/Main.java
@@ -0,0 +1,18 @@
+// ********************************************
+// Main.java
+//
+// Print a simple greeting to the world
+//
+// ********************************************
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+    }
+
+}
